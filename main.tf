6cb99b4099d103cacc8f1035117031d7d14b5987
Pascal Schur <pascal@uli-schur.de>
2025-06-13 15:05:09 +0200

    Add required providers block to specify provider versions

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17.0"
+    }
+  }
+}
 provider "kubernetes" {
   config_path = "./kubeconfig.yaml"
 }
