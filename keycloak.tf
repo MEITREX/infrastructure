10b62e94dcd1410dbc51dc0ab6c80c63ec138111
Pascal Schur <pascal@uli-schur.de>
2025-06-13 16:36:12 +0200

    Reenable use of custom image for gits theme

--- keycloak.tf
+++ keycloak.tf
@@ -2,8 +2,13 @@ resource "helm_release" "keycloak" {
   name       = "keycloak"
   repository = "oci://registry-1.docker.io/bitnamicharts"
   chart      = "keycloak"
-  version    = "24.7.4" 
+  version    = "24.7.4"
   namespace  = var.namespace
+
+  set {
+    name  = "global.security.allowInsecureImages"
+    value = "true"
+  }
 
   set {
     name  = "auth.adminUser"
@@ -14,58 +19,70 @@ resource "helm_release" "keycloak" {
     name  = "auth.adminPassword"
     value = var.keycloak_admin_pw
   }
+
   set {
     name  = "production"
     value = "true"
   }
+
   set {
     name  = "proxy"
     value = "edge"
   }
+
   set {
     name  = "httpRelativePath"
     value = "/keycloak/"
   }
-  /*
+
   set {
     name  = "image.pullPolicy"
     value = "Always"
   }
+
   set {
     name  = "image.registry"
     value = "ghcr.io"
   }
+
   set {
     name  = "image.repository"
-    value = "it-rex-platform/keycloak"
+    value = "meitrex/keycloak"
   }
+
   set {
     name  = "image.tag"
     value = "latest"
   }
-  */
+
   set {
     name  = "ingress.enabled"
     value = "true"
   }
+
   set {
     name  = "ingress.hostname"
     value = "dev.meitrex.de"
   }
+
   set {
     name  = "ingress.path"
     value = "/keycloak"
   }
+
   set {
     name  = "adminIngress.enabled"
     value = "true"
   }
+
   set {
     name  = "adminIngress.hostname"
     value = "dev.meitrex.de"
   }
+
   set {
     name  = "adminIngress.path"
     value = "/keycloak"
   }
+  
 }
