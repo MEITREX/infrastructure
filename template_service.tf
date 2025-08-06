016495de66640c43a63f68d2e0f739c1acd1c6dd
Pascal Schur <pascal@uli-schur.de>
2025-08-06 22:04:14 +0200

    Add template microservice tf file

---
+++ template_service.tf
@@ -0,0 +1,131 @@
+/*
+resource "kubernetes_deployment" "gits_<template>_service" {
+  depends_on = [helm_release.<template>_service_db, helm_release.dapr, helm_release.keel]
+  metadata {
+    name = "gits-<template>-service"
+    labels = {
+      app = "gits-<template>-service"
+    }
+    namespace = var.namespace
+    annotations = {
+      "keel.sh/policy"    = "force"
+      "keel.sh/match-tag" = "true"
+      "keel.sh/trigger"   = "poll"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "gits-<template>-service"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "gits-<template>-service"
+        }
+        annotations = {
+          "dapr.io/enabled"   = true
+          "dapr.io/enable-metrics" = true
+          "dapr.io/app-id"    = "<template>-service"
+          "dapr.io/app-port"  = 0001 
+          "dapr.io/http-port" = 0000
+        }
+      }
+
+      spec {
+        container {
+          image             = "ghcr.io/meitrex/<template>_service:latest"
+          image_pull_policy = "Always"
+
+          name = "gits-<template>-service"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "50m"
+              memory = "50Mi"
+            }
+          }
+
+          env {
+            name  = "SPRING_DATASOURCE_URL"
+            value = "jdbc:postgresql://<template>-service-db-postgresql:5432/<template>-service"
+          }
+
+          env {
+            name  = "SPRING_DATASOURCE_USERNAME"
+            value = "gits"
+          }
+
+          env {
+            name  = "SPRING_DATASOURCE_PASSWORD"
+            value = random_password.<template>_service_db_pass.result
+          }
+
+           liveness_probe {
+             http_get {
+               path = "/actuator/health/liveness"
+               port = 0001
+
+             }
+
+             initial_delay_seconds = 30
+             period_seconds        = 9
+           }
+
+           readiness_probe {
+             http_get {
+               path = "/actuator/health/readiness"
+               port = 0001
+
+             }
+
+             initial_delay_seconds = 30
+             period_seconds        = 9
+           }
+        }
+      }
+    }
+  }
+}
+
+resource "random_password" "<template>_service_db_pass" {
+  length  = 32
+  special = false
+}
+
+resource "helm_release" "<template>_service_db" {
+  name       = "<template>-service-db"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "postgresql"
+  namespace  = var.namespace
+
+  set {
+    name  = "global.postgresql.auth.database"
+    value = "<template>-service"
+  }
+
+  set {
+    name  = "postgres.auth.enablePostgresUser"
+    value = "false"
+  }
+
+  set {
+    name  = "global.postgresql.auth.username"
+    value = "gits"
+  }
+
+  set {
+    name  = "global.postgresql.auth.password"
+    value = random_password.<template>_service_db_pass.result
+  }
+}
+*/
