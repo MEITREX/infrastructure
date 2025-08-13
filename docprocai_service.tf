44be831e52cd66bff56c88d34fa28b7650912509
Pascal Schur <pascal@uli-schur.de>
2025-08-13 17:32:30 +0200

    Add DocProcAi Service

---
+++ docprocai_service.tf
@@ -0,0 +1,130 @@
+/*resource "kubernetes_deployment" "gits_docprocai_service" {
+  depends_on = [helm_release.docprocai_service_db, helm_release.dapr, helm_release.keel]
+  metadata {
+    name = "gits-docprocai-service"
+    labels = {
+      app = "gits-docprocai-service"
+    }
+    namespace = var.namespace
+    annotations = {
+      "keel.sh/policy"    = "force"
+      "keel.sh/match-tag" = "true"
+      "keel.sh/trigger"   = "poll"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "gits-docprocai-service"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "gits-docprocai-service"
+        }
+        annotations = {
+          "dapr.io/enabled"   = true
+          "dapr.io/enable-metrics" = true
+          "dapr.io/app-id"    = "docprocai-service"
+          "dapr.io/app-port"  = 9901
+          "dapr.io/http-port" = 9900
+        }
+      }
+
+      spec {
+        container {
+          image             = "ghcr.io/meitrex/docprocai_service:latest"
+          image_pull_policy = "Always"
+
+          name = "gits-docprocai-service"
+
+          resources {
+            limits = {
+              cpu    = "1"
+              memory = "1Gi"
+            }
+            requests = {
+              cpu    = "500m"
+              memory = "256Mi"
+            }
+          }
+
+          env {
+            name  = "SPRING_DATASOURCE_URL"
+            value = "jdbc:postgresql://docprocai-service-db-postgresql:5432/docprocai-service"
+          }
+
+          env {
+            name  = "SPRING_DATASOURCE_USERNAME"
+            value = "root"
+          }
+
+          env {
+            name  = "SPRING_DATASOURCE_PASSWORD"
+            value = "root"
+          }
+
+           liveness_probe {
+             http_get {
+               path = "/actuator/health/liveness"
+               port = 9901
+
+             }
+
+             initial_delay_seconds = 30
+             period_seconds        = 9
+           }
+
+           readiness_probe {
+             http_get {
+               path = "/actuator/health/readiness"
+               port = 9901
+
+             }
+
+             initial_delay_seconds = 30
+             period_seconds        = 9
+           }
+        }
+      }
+    }
+  }
+}
+
+resource "random_password" "docprocai_service_db_pass" {
+  length  = 32
+  special = false
+}
+
+resource "helm_release" "docprocai_service_db" {
+  name       = "docprocai-service-db"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "postgresql"
+  namespace  = var.namespace
+
+  set {
+    name  = "global.postgresql.auth.database"
+    value = "docprocai-service"
+  }
+
+  set {
+    name  = "postgres.auth.enablePostgresUser"
+    value = "false"
+  }
+
+  set {
+    name  = "global.postgresql.auth.username"
+    value = "gits"
+  }
+
+  set {
+    name  = "global.postgresql.auth.password"
+    value = random_password.docprocai_service_db_pass.result
+  }
+}
+*/
