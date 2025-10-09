1f3ee5968d38fc25031af184189e752327514e83
Pascal Schur <pascal@uli-schur.de>
2025-10-09 14:20:56 +0200

    Add missing environment variables

--- notification_service.tf
+++ notification_service.tf
@@ -69,6 +69,16 @@ resource "kubernetes_deployment" "gits_notification_service" {
             value = random_password.notification_service_db_pass.result
           }
 
+          env {
+            name  = "COURSE_SERVICE_URL"
+            value = "http://localhost:3500/v1.0/invoke/course-service/method/graphql"
+          }
+
+          env {
+            name  = "USER_SERVICE_URL"
+            value = "http://localhost:3500/v1.0/invoke/user-service/method/graphql"
+          }
+
           liveness_probe {
             http_get {
               path = "/actuator/health/liveness"
